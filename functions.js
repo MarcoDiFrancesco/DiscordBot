0cf1ab2f988c5ef87ea7383281c5bba87043cef7
MarcoDiFrancesco <marcodifran@gmail.com>
2021-03-28 16:48:27 +0200

    Add register commands function

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+import fetch, { Headers } from "node-fetch";
+
 export const cleanMessage = (message) => {
   // e.g. -> ?command arg1 arg2 arg3
   let message_str = message.content;
@@ -14,4 +16,40 @@ export const cleanMessage = (message) => {
   // Make command lowercase
   message_str[0] = message_str[0].toLowerCase();
   return message_str;
+};
+
+const commandList = [
+  {
+    name: "aggiungi",
+    description: "Iscrivi un player al torneo",
+    options: [
+      {
+        name: "tag",
+        description: "Tag del player da aggiungere, per esempio #ABCDEFGH",
+        type: 3,
+        required: true,
+      },
+    ],
+  },
+];
+
+export const registerCommands = async () => {
+  const url = `https://discord.com/api/v8/applications/${process.env.DISCORD_ID}/commands`;
+
+  for (const json of commandList) {
+    const headers = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      },
+      body: JSON.stringify(json),
+    };
+    const res = await fetch(url, headers);
+    if (res.status !== 200) {
+      console.error(
+        `Error in registering commands, status: ${res.status}, status text: ${res.statusText}`
+      );
+    }
+  }
 };
